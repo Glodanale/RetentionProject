3cd2c0b92c0d5cd58a9ba231aa857a9b5d9ca4f8
Glodanale <111537803+Glodanale@users.noreply.github.com>
2023-04-08 12:54:13 -0500

    Delete Difference_In_GPAs.sql

--- Difference_In_GPAs.sql
+++
@@ -1,4 +0,0 @@
-Select HighSchool, count(HighSchool) Count, round(avg(HIGH_SCHOOL_GPA), 2) HSGPA, round(avg(1st_College_Semester_GPA), 2) ColGPA, round(avg(HIGH_SCHOOL_GPA) - avg(1st_College_Semester_GPA), 2) difGPA
-from retensiondata
-group by HighSchool
-order by difGPA desc
