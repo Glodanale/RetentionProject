a1fa82f543de6161d836842d5b7dd3e379bc38d4
Glodanale <111537803+Glodanale@users.noreply.github.com>
2023-04-14 15:07:10 -0500

    Add files via upload

---
+++ College_of_FA_switches.sql
@@ -0,0 +1,5 @@
+Select COLLEGES1, count(COLLEGES1) CountSwitched, count(Spring_1_Retention) CountStayed
+from retensiondata
+where college_of_FA = 1 and college_change = 1 and Spring_1_Retention = 1
+group by COLLEGES1
+order by CountSwitched Desc
